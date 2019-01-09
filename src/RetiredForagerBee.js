53ecc26bdd6c1cd418ab86a53696d89f0d5cdd98
KweonMo Koo <mikekoo2010@gmail.com>
2019-01-09 12:32:10 -0800

    Completed 6ees6ees6ees

--- src/RetiredForagerBee.js
+++ src/RetiredForagerBee.js
@@ -1,3 +1,18 @@
-class RetiredForagerBee {
+class RetiredForagerBee extends ForagerBee {
   // TODO..
+  constructor() {
+    super();
+    this.age = 40;
+    this.color = 'grey';
+    this.job = 'gamble';
+    this.canFly = false;
+  }
+
+  forage() {
+    return "I am too old, let me play cards instead";
+  }
+
+  gamble() {
+    this.treasureChest.push('treasure');
+  }
 };
